7a5cce600ec0828ec98ae16c9c20a68661938a67
nkuek <nkuek1@gmail.com>
2021-02-20 03:20:41 -0500

    about me transitions

--- frontend/src/components/About/index.js
+++ frontend/src/components/About/index.js
@@ -3,19 +3,25 @@ const About = () => {
     return (
         <div className="aboutPageWrapper">
             <div className="aboutPage">
-                <h1 className="aboutHeader">Nick Kuek</h1>
-                <p className="aboutSubheader">
+                <h1 className="aboutHeader fade-in">Nick Kuek</h1>
+                <p className="aboutSubheader fade-in">
                     Full Stack Developer • App Academy Student • Software
                     Developer
                 </p>
-                <div className="socialsContainer">
-                    <a href="http://github.com/nkuek">
+                <div className="socialsContainer fade-in">
+                    <a
+                        href="http://github.com/nkuek"
+                        className="github fade-in"
+                    >
                         <i className="fab fa-github fa-3x"></i>
                     </a>
-                    <a href="mailto:nkuek1@gmail.com">
+                    <a href="mailto:nkuek1@gmail.com" className="email fade-in">
                         <i className="fas fa-envelope fa-3x"></i>
                     </a>
-                    <a href="www.linkedin.com/in/nick-kuek">
+                    <a
+                        href="www.linkedin.com/in/nick-kuek"
+                        className="linkedin fade-in"
+                    >
                         <i className="fab fa-linkedin fa-3x"></i>
                     </a>
                 </div>
