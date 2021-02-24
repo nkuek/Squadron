b3a10c6a273af98f10db3f5b41c177b37f9e2305
nkuek <nkuek1@gmail.com>
2021-02-24 16:46:35 -0500

    fixed styling

--- frontend/src/components/NewSquadForm/index.js
+++ frontend/src/components/NewSquadForm/index.js
@@ -11,32 +11,47 @@ const NewSquadForm = () => {
     return (
         <>
             <div className="newSquadFormWrapper">
-                <div className="newSquadFormContainer">
-                    <div className="closeNewSquadFormContainer">
-                        <Link to="/squads" className="closeNewSquadForm">
-                            <i className="fas fa-arrow-left closeFormArrow"></i>
-                        </Link>
-                    </div>
-                    <span className="newSquadFormHeaderContainer">
-                        <h1 className="newSquadFormHeader">Create Squad</h1>
-                    </span>
-                    <div className="newSquadFormSquadNameContainer">
-                        <span
-                            style={{ color: 'white' }}
-                            className="newSquadFormSquadName"
-                        >
-                            Squad Name
+                <div className="newSquadContainer">
+                    <div className="newSquadFormContainer">
+                        <div className="closeNewSquadFormContainer">
+                            <Link to="/squads" className="closeNewSquadForm">
+                                <i className="fas fa-arrow-left closeFormArrow">
+                                    {' Back'}
+                                </i>
+                            </Link>
+                        </div>
+                        <span className="newSquadFormHeaderContainer">
+                            <h1 className="newSquadFormHeader">Create Squad</h1>
                         </span>
-                        <input
-                            type="text"
-                            value={squadName}
-                            onChange={(e) => setSquadName(e.target.value)}
-                        ></input>
+                        <div className="newSquadFormSquadNameContainer">
+                            <span
+                                style={{ color: 'white' }}
+                                className="newSquadFormSquadName"
+                            >
+                                Squad Name
+                            </span>
+                            <input
+                                type="text"
+                                value={squadName}
+                                onChange={(e) => setSquadName(e.target.value)}
+                            ></input>
+                        </div>
                     </div>
-                </div>
 
-                <div className="newSquadFormPreviewContainer">
-                    <h1>{squadName}</h1>
+                    <div className="newSquadFormPreviewWrapper">
+                        <div className="squadOuterContainer">
+                            <div className="squadInnerContainer">
+                                <div className="newSquadFormPreviewHeader">
+                                    <p className="preview">Preview</p>
+                                </div>
+                                <div className="squadName">
+                                    <h1>
+                                        {!squadName ? 'Squad Name' : squadName}
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
