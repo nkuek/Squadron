572ce5d49651c5546e5723a975d1d9a2c45872c0
nkuek <nkuek1@gmail.com>
2021-02-26 11:56:57 -0500

    squads show on profile pages now

--- frontend/src/components/UserProfile/UserSquads/index.js
+++ frontend/src/components/UserProfile/UserSquads/index.js
@@ -6,16 +6,24 @@ const UserSquads = () => {
     const userState = useSelector((state) => state.session.user);
 
     const { username } = useParams();
-    let profileName =
-        userState.username === username ? 'Your' : `${userState.username}'s`;
 
-    if (userState.username === username) profileName = 'Your';
+    let profileName = userState.username === username ? 'You' : `${username}`;
 
     return (
         // {!squads ? <div className='noSquads'></div>}
         <>
             <div className="userSquadListWrapper">
-                <div className="userSquadListContainer"></div>
+                <div className="userSquadListContainer">
+                    {squads[0].Squads.map((squad, idx) => (
+                        <div key={idx} className="userSquadsContainer">
+                            <p className="userSquadName">{squad.squadName}</p>
+                            <p className="userSquadPrimaryType">
+                                {squad.primaryType}
+                            </p>
+                            <p className="userSquadCaptain">{profileName}</p>
+                        </div>
+                    ))}
+                </div>
             </div>
         </>
     );
