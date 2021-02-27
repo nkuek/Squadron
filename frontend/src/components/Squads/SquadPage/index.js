cdf517b7ac56decaff533e9262ad1932babf6fc1
nkuek <nkuek1@gmail.com>
2021-02-27 18:31:15 -0500

    about page

--- frontend/src/components/Squads/SquadPage/index.js
+++ frontend/src/components/Squads/SquadPage/index.js
@@ -1,5 +1,31 @@
 const SquadPage = () => {
-    return <div></div>;
+    return (
+        <div className="squadPageWrapper">
+            <div className="squadPageContainer">
+                <div className="squadPageHeaderContainer">
+                    <p className="squadPageHeader">Squads</p>
+                </div>
+                <div className="squadsListContainer">
+                    <div className="squadsListHeaderContainer">
+                        <p className="squadType">Gaming</p>
+                    </div>
+                    <div className="squadsListBodyContainer"></div>
+                </div>
+                <div className="squadsListContainer">
+                    <div className="squadsListHeaderContainer">
+                        <p className="squadType">Social</p>
+                    </div>
+                    <div className="squadsListBodyContainer"></div>
+                </div>
+                <div className="squadsListContainer">
+                    <div className="squadsListHeaderContainer">
+                        <p className="squadType">Trading</p>
+                        <div className="squadsListBodyContainer"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default SquadPage;
