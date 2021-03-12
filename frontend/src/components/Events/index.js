d37e742f7e9e185fe91ae5777a718769e96cd301
nkuek <nkuek1@gmail.com>
2021-03-12 03:06:29 -0500

    events page placeholder

--- frontend/src/components/Events/index.js
+++ frontend/src/components/Events/index.js
@@ -1,13 +1,16 @@
 import { useSelector } from 'react-redux';
 import { Redirect, Route } from 'react-router-dom';
 import Event from './Event';
+import './events.css';
 
 const Events = () => {
     const user = useSelector((state) => state.session.user);
     if (!user) return <Redirect to="/login" />;
     return (
         <>
-            <div>hello</div>
+            <div className="eventsPageContainer">
+                <div className="eventsPageHeader">Feature Coming Soon!</div>
+            </div>
             <Route path="/events/:eventId">
                 <Event />
             </Route>
